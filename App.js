f12227d7af69cadd395bb770781089ca10a4c24c
DaffaMuflih <daffa.muflih02@gmail.com>
2025-02-14 11:41:09 +0700

    Tugas Lab

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <Text style={styles.blueText}>Nama Nama Teman</Text>
+
+        <Image source={require('./assets/aceng.png')} style={styles.image} />
+        <Text style={styles.blueText}>Albie Fachri</Text>
+        <Image source={require('./assets/arap.png')} style={styles.image} />
+        <Text style={styles.blueText}>Muhammad Faraj</Text>
+        <Image source={require('./assets/damu.png')} style={styles.image} />
+        <Text style={styles.blueText}>Daffa Muflih</Text>
+        <Image source={require('./assets/devan.png')} style={styles.image} />
+        <Text style={styles.blueText}>Devan Jonathan</Text>
+        <Image source={require('./assets/ilham.png')} style={styles.image} />
+        <Text style={styles.blueText}>Ilham God</Text>
+        <Image source={require('./assets/juan.png')} style={styles.image} />
+        <Text style={styles.blueText}>Juan Andreas</Text>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 20,
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 50,
+    height: 50,
+    marginVertical: 10,
+  },
+  blueText: {
+    color: 'blue',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
